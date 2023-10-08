ba6ff03aa765a117d51780cbcd5db763b38dae0f
ROMY SOOSAI <129157454+rowohme0047@users.noreply.github.com>
2023-10-08 20:43:03 +0530

    Add files via upload

---
+++ QuizGame/src/main/java/com/example/entity/Question.java
@@ -0,0 +1,32 @@
+package com.example.entity;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Question {
+
+
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Integer id;
+	    private String questionTitle;
+	    private String option1;
+	    private String option2;
+	    private String option3;
+	    private String option4;
+	    private String rightAnswer;
+	    private String difficultylevel;
+	    private String category;
+
+
+	}
+
---
+++ QuizGame/src/main/java/com/example/entity/Response.java
@@ -0,0 +1,15 @@
+package com.example.entity;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+
+	    private Integer id;
+	    private String response;
+
+			// TODO Auto-generated method stub
+		
+}
